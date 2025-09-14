1fa006408bf675d6b50f1c4035915bc5a4d430bd
nabin12468 <nabindamase12345@gmail.com>
2025-09-14 14:12:51 +0545

    string

---
+++ string.py
@@ -0,0 +1,33 @@
+"""str="this"
+len1 = len(str)
+print(len1)
+
+str1="is"
+len2 = len(str1)
+print(len2)
+final = str + " " + str1
+print(str,str1) """
+
+# indexing
+'''str = "this is string"
+print(str[5])'''
+
+# slicing
+'''Accesing parts of a string
+str = "this is string"
+print(str[1:4])
+print(str[5:7])
+print(str[7:14])
+print(str[0:])
+print(str[:10])'''
+
+# negative indexing
+
+'''str = "this is string"
+print(str[-1:-5])
+print(str[-5:-1]) '''
+
+
+
+
+
