5575f9c14f2ea200a818f2fa7c8dd0392319a1b7
nabin12468 <nabindamase12345@gmail.com>
2025-11-05 15:23:50 +0545

    numbera-board

--- game.py
+++ game.py
@@ -1,4 +1,4 @@
-class TictacToe
+class TicTacToe:
  def __init__(self):
         self.board = [' ' for _ in range(9)]  # A list to hold the board state
         self.current_winner = None  # Keep track of the winner!
@@ -7,3 +7,12 @@ def print_board(self):
         # This is just getting the rows
         for row in [self.board[i*3:(i+1)*3] for i in range(3)]:
             print('| ' + ' | '.join(row) + ' |')
+
+
+@staticmethod
+def print_board_nums():
+        # 0 | 1 | 2 etc (tells us what number corresponds to what box)
+        number_board = [[str(i) for i in range(j*3, (j+1)*3)] for j in range(3)]
+        for row in number_board:
+            print('| ' + ' | '.join(row) + ' |')
+     
