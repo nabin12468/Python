d840f24db5c46c56c98d246358ccb58935e48754
nabin12468 <nabindamase12345@gmail.com>
2025-11-06 16:47:06 +0545

    board

--- game.py
+++ game.py
@@ -16,3 +16,9 @@ def print_board_nums():
         for row in number_board:
             print('| ' + ' | '.join(row) + ' |')
      
+@staticmethod
+def print_board_nums():
+        # 0 | 1 | 2 etc (tells us what number corresponds to what box)
+        number_board = [[str(i) for i in range(j*3, (j+1)*3)] for j in range(3)]
+        for row in number_board:
+            print('| ' + ' | '.join(row) + ' |')
