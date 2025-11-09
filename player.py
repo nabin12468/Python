245b81b3ef51f5c351964af7eb99a052e6e91cd7
nabin12468 <nabindamase12345@gmail.com>
2025-11-09 18:32:49 +0545

    try

--- player.py
+++ player.py
@@ -21,4 +21,15 @@ class RandomComputerPlayer(Player):
         super().__init__(letter)
 
     def get_move(self, game):
-        pass
+        valid_square = False
+        val = None
+        while not valid_square:
+            square = input(self.letter + '\'s turn. Input move (0-8): ')
+            try:
+                val = int(square)
+                if val not in game.available_moves():
+                    raise ValueError
+                valid_square = True
+            except ValueError:
+                print('Invalid square. Try again.')
+        return val
